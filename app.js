3d84f10505222361f9177476e8352d427974787c
khunliang <94458459+khunliang@users.noreply.github.com>
2023-03-03 20:58:32 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-console.log("hello")
-var name = "Liang"
-var surname = "Duangsrimaneerat"
-var age = 10
-console.log(age+20*2)
-console.log(typeof age)
-var pi = 3.14
-console.log(pi+2)
-alert(age)
+function toCelsius(fahrenheit)
+{
+    let value = (fahrenheit - 32) * 5 / 9
+    return value.toFixed(2) + "˚C"
+}
 
+function toFahrenheit(celsius)
+{
+    let value = (celsius * 9) / 5 + 32
+    return value.toFixed(2) + "°F"
+}
 
+function display(elementID, value)
+{
+    document.getElementById(elementID).innerHTML = "<b>"+value+"</b>"
+}
+
+function toCelsiusProgram(value)
+{
+    alert(toCelsius(value))
+}
+
+function toFahrenheitProgram(value)
+{
+    alert(toFahrenheit(value))
+}
