f033f691c31bf7e5dafb576afb178201a63d3d4f
khunliang <94458459+khunliang@users.noreply.github.com>
2023-03-03 16:04:07 +0700

    fahrenheit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hello")
+var name = "Liang"
+var surname = "Duangsrimaneerat"
+var age = 10
+console.log(age+20*2)
+console.log(typeof age)
+var pi = 3.14
+console.log(pi+2)
+alert(age)
+
+
